2c3bffc4cadbded9bd19b515ee9aa880e25b0948
tae.panyaphoem <tae.panyaphoem@everymatrix.com>
2025-03-30 05:03:36 +0700

    fix response for very very small

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-right-heart-fill" viewBox="0 0 16 16">
             <path d="M16 2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9.586a1 1 0 0 1 .707.293l2.853 2.853a.5.5 0 0 0 .854-.353zM8 3.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132"/>
         </svg>
-        <label class="w-80 text-center cute-font-regular text-xl"> Song to my future girlfriend ✨</label>
+        <label class="header-text xl:w-80 text-center cute-font-regular text-sm xl:text-xl"> Song to my future girlfriend ✨</label>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-music-player" viewBox="0 0 16 16">
             <path d="M4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1zm1 0v3h6V3zm3 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
             <path d="M11 11a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-3 2a2 2 0 1 0 0-4 2 2 0 0 0 0 4"/>
@@ -23,7 +23,7 @@
             <div class="blue bar"></div>
             <div class="violet bar"></div>
         </div>
-        <img class="w-2xs shadow-md aspect-1/1 object-cover rounded-lg mx-auto" alt="The project logo" src={'/couple_image.jpg'} />
+        <img class="w-3xs xl:w-2xs shadow-md aspect-1/1 object-cover rounded-lg mx-auto" alt="The project logo" src={'/couple_image.jpg'} />
         <AudioControls audioSrc="/song.mp3"></AudioControls>
         <p class="text-center cute-font-regular text-l text-purple-800">
             You will get the best version of me 💖,
@@ -31,7 +31,7 @@
         <p class="cute-text-regular text-l text-center text-fuchsia-600">🎶 perhaps even this melody</p>
         <a href="https://www.youtube.com/watch?v=MaeX5dV0njY" target="_blank" >
             <div class="w-full mx-auto flex flex-row justify-content-center items-center" >
-                <span class="mr-1 sriracha-regular hover:text-pink-400 text-pink-800">ถ้าโลกนี้ไม่ใจดีกับเธอ เดี๋ยวเราใจดีกับเธอแทน</span>
+                <span class="mr-1 sriracha-regular hover:text-pink-400 text-pink-800 text-sm xl:text-base">ถ้าโลกนี้ไม่ใจดีกับเธอ เดี๋ยวเราใจดีกับเธอแทน</span>
                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="40px" height="40px" viewBox="0 -7 48 48" version="1.1">
                     <g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                         <g id="Color-" transform="translate(-200.000000, -368.000000)" fill="#CE1312">
