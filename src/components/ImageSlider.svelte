781fcfbd40629e3d7177b76e619ccf6d14dde1ec
tae.panyaphoem <tae.panyaphoem@everymatrix.com>
2025-03-30 21:43:44 +0700

    Change slide logic and added more images

--- src/components/ImageSlider.svelte
+++ src/components/ImageSlider.svelte
@@ -1,29 +1,27 @@
 <script>
     import { onMount } from "svelte";
     let secToMin = 1000;
-    let { delay = 7 } = $props();
+    let { delay = 5 } = $props();
     delay = delay * secToMin;
-    let images = ['/couple/couple_image.jpg', '/couple/0c4968db9ef82b81b8098cb98a74d5e9.jpg']
+    let images = ['/couple/couple_image.jpg']
+    let imageSrc = $state(images[0]);
+    let currentImageIndex = 0;
 
     onMount(async () => {
         const res = await fetch('/api/couple-images');
         images = await res.json();
+        console.log('images.length : ', images.length);
+        if(images.length > 1){
+            setTimeout(() => {
+                setInterval(changeImage, delay); // Change image every 2 seconds
+            }, 50)
+        }
     });
-    console.log('all images : ', images)
-
-
-    let imageSrc = $state(images[0]);
-    let maxImage = images.length;
-    let currentImageIndex = 0;
-
+    
     function changeImage() {
         currentImageIndex = (currentImageIndex + 1) % images.length;
         imageSrc = images[currentImageIndex];
     }
-
-    setTimeout(() => {
-        setInterval(changeImage, delay); // Change image every 2 seconds
-    }, 1000)
 
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-right-heart-fill" viewBox="0 0 16 16">
             <path d="M16 2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9.586a1 1 0 0 1 .707.293l2.853 2.853a.5.5 0 0 0 .854-.353zM8 3.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132"/>
         </svg>
-        <label class="header-text xl:w-80 text-center cute-font-regular text-sm xl:text-xl"> Song to my future girlfriend ✨</label>
+        <label class="header-text xl:w-80 text-center cute-font-regular text-sm xl:text-xl"> To my future girlfriend ✨</label>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-music-player" viewBox="0 0 16 16">
             <path d="M4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1zm1 0v3h6V3zm3 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
             <path d="M11 11a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-3 2a2 2 0 1 0 0-4 2 2 0 0 0 0 4"/>
